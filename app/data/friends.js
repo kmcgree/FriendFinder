e0b99a850670b706f774e8d4ab4d3474dd24e5be
Kane McGree <kmcgree14@gmail.com>
2018-10-06 17:28:04 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+const friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Ben",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            3,
+            1,
+            5,
+            1,
+            4,
+            3,
+            2,
+            4,
+            3
+          ]
+      }
+
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,48 @@
+
+const friends = require('../data/friends');
+
+
+module.exports = (app) => {
+
+    app.get('/api/friends', (req, res) => {
+        res.json(friends);
+    });
+
+    app.post('app/friends', (req, res) => {
+        let userScores = req.body.scores;
+        let difference = 1000;
+        let bestMatchName = '';
+        let bestMatchPhoto = '';
+
+        friends.forEach((person) => {
+            let scores = [];
+            let totalDifference = 1000;
+
+            function add(a, b) {
+                return a + b;
+            }
+
+            for (i = 0; i < person.scores.length; i++) {
+                matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(person.scores[i])));
+            }
+
+            totalDifference = scores.reduce(add, 0);
+
+            if (totalDifference < difference) {
+                difference = totalDifference;
+
+                bestMatchName = person.name;
+                bestMatchPhoto = person.photo;
+            }
+        });
+
+        res.json({
+            name: bestMatchName,
+            photo: bestMatchPhoto
+        });
+
+        friends.push(req.body);
+    });
+
+
+};
